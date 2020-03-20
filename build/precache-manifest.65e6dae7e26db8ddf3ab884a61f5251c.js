6ee95d907317dfc34bf98334b8bb30f010495c21
Ting A Lin <tingalin@Tings-MacBook-Air.local>
2020-03-20 17:30:06 +0700

    v1

---
+++ build/precache-manifest.65e6dae7e26db8ddf3ab884a61f5251c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a492a7295bf54a8e9aad7d49a3dbd4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57672d9047266ca17263",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "1221b469c72e7dda2b9a",
+    "url": "/static/css/main.829c451d.chunk.css"
+  },
+  {
+    "revision": "57672d9047266ca17263",
+    "url": "/static/js/2.85018251.chunk.js"
+  },
+  {
+    "revision": "1221b469c72e7dda2b9a",
+    "url": "/static/js/main.36378b38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
